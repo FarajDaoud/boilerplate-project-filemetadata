461b84650c7b8e67704bbc0e24f73982aa675447
Faraj Daoud <faraj.daoud@gmail.com>
2019-01-29 12:14:29 -0500

    Finished Project

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
-var app = express();
+let app = express();
+
+//app.use(fileUpload());
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -18,6 +23,18 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
+//upfile is the name of the file input field. 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file);
+  let file = req.file;
+  if (Object.keys(file).length == 0) {
+    return res.status(400).send({Error:'No files were uploaded.'});
+  }else{
+    res.send({name: file.originalname, type: file.mimetype, size: file.size});
+  }
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
